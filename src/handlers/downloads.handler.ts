4e3cac394adc2799000e1e168142535d0b5b87f4
blurrrb <17aakashsingh1999@gmail.com>
2022-05-21 12:22:00 +0530

    Refactoring

--- src/handlers/downloads.handler.ts
+++ src/handlers/downloads.handler.ts
@@ -1,7 +1,19 @@
+const hostRegex = /https?:\/\/([^\/]*)(\/.*)?/;
+
+function getHost(url: string): string {
+  const match = url.match(hostRegex);
+  if (!match) return "";
+  return match[1];
+}
+
 export default function getHandler(url: string) {
   return async (request: Request) => {
     try {
       const proxyRequest = new Request(url, request);
+      // rewrite host information
+      proxyRequest.headers.set("Host", getHost(url));
+      console.log("proxy", proxyRequest.headers.get("Host"));
+
       const response = await fetch(proxyRequest);
       const { readable, writable } = new TransformStream();
       response.body?.pipeTo(writable);
